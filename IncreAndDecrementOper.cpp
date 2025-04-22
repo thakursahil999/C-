27a6b6e0322dbca6538f50d5253b9b03c34ddb58
thakursahil999 <thakursahil9302@gmail.com>
2025-04-22 22:49:48 +0530

    cpp

--- IncreAndDecrementOper.cpp
+++ IncreAndDecrementOper.cpp
@@ -19,4 +19,36 @@ int main()
     cout<<--x<<endl;
     cout<<x;
 
+
+    // **********Practice Ques**********
+
+    cout<<"\n\n********\n";
+    int a1,b1=1;
+    a1=10;
+    if(++a)
+    {
+        cout<<b1;
+    }
+    else{
+        cout<<++b1;
+    }
+
+    cout<<endl;
+    int a2=1;
+    int b2=2;
+
+    if(a2-- > 0 && ++b2 > 2){
+        cout<<"if Statement";
+    }
+    else{
+        cout<<"else statement\n";
+    }
+    cout<<a2<<" "<<b2<<endl;
+
+    int a3=1;
+    int b3=a3++;
+    int c3=++a3;
+    cout<<a3<<endl;
+    cout<<b3<<endl;
+    cout<<c3;
 }
