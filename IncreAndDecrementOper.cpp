2a85aae1e45e3daa48a762ebfd6f4be7533ff081
thakursahil999 <thakursahil9302@gmail.com>
2025-04-22 22:35:39 +0530

    new cpp file

---
+++ IncreAndDecrementOper.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // *********Increment Operator**********
+    int a=10;
+    cout<<a++<<endl; //first the value print and than increment post increment
+    cout<<a<<endl;
+
+    cout<<++a<<endl;//first the value increment and than print pre increment
+    cout<<a<<endl;
+
+    // **********Decrement Operator***********
+
+    int x=10;
+    cout<<x--<<endl;
+    cout<<x<<endl;
+
+    cout<<--x<<endl;
+    cout<<x;
+
+}
